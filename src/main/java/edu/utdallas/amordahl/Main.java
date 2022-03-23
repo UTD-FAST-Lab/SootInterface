54730937c0c11c06f45190d7c93cdec4185dc614
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-03-23 16:29:22 -0500

    Fixes and updates

--- src/main/java/edu/utdallas/amordahl/Main.java
+++ src/main/java/edu/utdallas/amordahl/Main.java
@@ -4,10 +4,23 @@ import soot.PackManager;
 import soot.Transform;
 import edu.utdallas.amordahl.transformers.CallgraphPrinter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        String output = "callgraph.tsv";
+        ArrayList<String> args_as_list = new ArrayList<String>(Arrays.asList(args));
+        if (args_as_list.contains("--callgraph-output")) {
+            int index = args_as_list.indexOf("--callgraph-output");
+            output = args_as_list.get(index + 1);
+            args_as_list.remove(index);
+            args_as_list.remove(index);
+            args = args_as_list.toArray(new String[] {});
+        }
         PackManager.v().getPack("wjtp").add(
-                new Transform("wjtp.callgraphPrinter", new CallgraphPrinter())
+                new Transform("wjtp.print-callgraph", new CallgraphPrinter(output))
         );
         soot.Main.main(args);
     }
--- src/main/java/edu/utdallas/amordahl/transformers/CallgraphPrinter.java
+++ src/main/java/edu/utdallas/amordahl/transformers/CallgraphPrinter.java
@@ -1,19 +1,41 @@
 package edu.utdallas.amordahl.transformers;
 
+import polyglot.ast.Call;
 import soot.Scene;
 import soot.SceneTransformer;
+import soot.jimple.toolkits.callgraph.CallGraph;
 import soot.jimple.toolkits.callgraph.Edge;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 import java.util.function.Consumer;
 
 public class CallgraphPrinter extends SceneTransformer {
+    private final String output;
+
+    public CallgraphPrinter(String output) {
+        this.output = output;
+    }
     protected void internalTransform(String s, Map<String, String> map) {
-        Scene.v().getCallGraph().forEach(edge -> System.out.println(String.format("%s\t%s\t%s\t%s\t%s",
-                edge.src(),
-                edge.srcUnit(),
-                edge.src().context(),
-                edge.tgt().method(),
-                edge.tgt().context())));
+        try {
+            FileWriter fw = new FileWriter(this.output);
+            fw.write("caller\tcallsite\tcaller_context\ttarget\ttarget_context\n");
+            Scene.v().getCallGraph().forEach(edge -> {
+                try {
+                    fw.write(String.format("%s\t%s\t%s\t%s\t%s\n",
+                        edge.src(),
+                        edge.srcUnit(),
+                        edge.src().context(),
+                        edge.tgt().method(),
+                        edge.tgt().context()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

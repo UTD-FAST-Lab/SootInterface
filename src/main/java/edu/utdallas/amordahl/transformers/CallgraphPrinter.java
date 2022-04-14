6ba5b2e91cdb0b209a854bedc035eb0bb049fbb6
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-13 19:17:59 -0500

    Update CallgraphPrinter.java

--- src/main/java/edu/utdallas/amordahl/transformers/CallgraphPrinter.java
+++ src/main/java/edu/utdallas/amordahl/transformers/CallgraphPrinter.java
@@ -3,6 +3,11 @@ package edu.utdallas.amordahl.transformers;
 import polyglot.ast.Call;
 import soot.Scene;
 import soot.SceneTransformer;
+import soot.jimple.internal.JAssignStmt;
+import soot.jimple.internal.JInvokeStmt;
+import soot.jimple.internal.JNewArrayExpr;
+import soot.jimple.internal.JStaticInvokeExpr;
+import soot.jimple.spark.geom.dataRep.CgEdge;
 import soot.jimple.toolkits.callgraph.CallGraph;
 import soot.jimple.toolkits.callgraph.Edge;
 
@@ -28,6 +33,7 @@ public class CallgraphPrinter extends SceneTransformer {
                 try {
                     fw.write(String.format("%s\t%s\t%s\t%s\t%s\n",
                         edge.src(),
+                        String.format("<%s: %s>/%s/%s", edge.src().getDeclaringClass(), edge.src().toString(), getMethodCall(edge), 3),
                         this.srcLineNumbers ?
                                 String.format("%s:%d", edge.src().method().getDeclaringClass().toString().replace(".","/"),
                                         edge.srcUnit().getJavaSourceStartLineNumber()) :
@@ -37,11 +43,32 @@ public class CallgraphPrinter extends SceneTransformer {
                         edge.tgt().context()));
                 } catch (IOException | NullPointerException e) {
                     e.printStackTrace();
+                    System.err.println("Could not process edge " + edge.toString());
                 }
             });
             fw.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private String getMethodCall(Edge edge) {
+        if (edge.srcUnit() instanceof JInvokeStmt) {
+            JInvokeStmt jis = (JInvokeStmt) edge.srcUnit();
+            return String.format("%s.%s", jis.getInvokeExpr().getMethod().getDeclaringClass().toString(),
+                    jis.getInvokeExpr().getMethod().getName());
+        } else if (edge.srcUnit() instanceof JAssignStmt) {
+            JAssignStmt jas = (JAssignStmt) edge.srcUnit();
+            if (jas.getRightOp() instanceof JStaticInvokeExpr) {
+                return String.format("%s.%s",
+                        ((JStaticInvokeExpr)jas.getRightOp()).getMethod().getDeclaringClass().toString(),
+                        ((JStaticInvokeExpr)jas.getRightOp()).getMethod().getName());
+            }
+            else {
+                return jas.getRightOpBox().getValue().toString();
+            }
+        } else {
+            throw new RuntimeException("Could not figure out what the type of unit " + edge.srcUnit());
         }
     }
 }
